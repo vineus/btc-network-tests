fe4f01858788003c9b57f941fbcab11d41fe32d6
Vincent de Lagabbe <vineus@gmail.com>
2014-09-16 17:59:20 +0200

    some cleanup

--- msgutils.js
+++ msgutils.js
@@ -1,5 +1,4 @@
 var hash = require('hash.js');
-var buffertools = require('buffertools');
 var net = require('net');
 var crypto = require('crypto');
 
@@ -13,24 +12,29 @@ function writeUInt64LE(buffer, value, offset) {
 
 function buildMessage(command, payload) {
   var checksum = new Buffer(hash.sha256().update(hash.sha256().update(payload).digest()).digest());
-  var message = new Buffer(24);
+  var message = new Buffer(24 + payload.length);
   message.fill(0);
-  message.writeUInt32LE(MAGICTEST, 0);
-  message.write(command, 4);
-  message.writeUInt32LE(payload.length, 16);
-  checksum.copy(message, 20, 0, 4);
-  return buffertools.concat(message, payload);
+  message.writeUInt32LE(MAGICTEST, 0); // magic number
+  message.write(command, 4); // command name
+  message.writeUInt32LE(payload.length, 16); // payload length
+  checksum.copy(message, 20, 0, 4); // payload checksum
+  payload.copy(message, 24); // payload
+  return message;
 }
 
-function buildNetAddr(port) {
+function buildNetAddr(ipAddress, port) {
   var netAddr = new Buffer(26);
   netAddr.fill(0);
-  writeUInt64LE(netAddr, 1, 0); //services
-  netAddr.writeUInt8(127, 8 + 12);
-  netAddr.writeUInt8(0, 8 + 12 + 1);
-  netAddr.writeUInt8(0, 8 + 12 + 2);
-  netAddr.writeUInt8(1, 8 + 12 + 3);
-  netAddr.writeUInt16BE(port, 8 + 12 + 4);
+  var pos = 0;
+  writeUInt64LE(netAddr, 1, pos); //services
+  pos += 8;
+  var ipPos = pos + 15;
+  ipAddress.reverse().forEach(function(b) {
+    netAddr.writeUInt8(b, ipPos); // IP bytes
+    ipPos -= 1;
+  });
+  pos += 16;
+  netAddr.writeUInt16BE(port, pos);
   return netAddr;
 }
 
@@ -46,20 +50,22 @@ function buildVersion() {
   var timeStamp = Math.round(new Date().getTime() / 1000);
   writeUInt64LE(versionMessage, timeStamp, pos); // timestamp
   pos += 8;
-  var ip = buildNetAddr(18333);
-  ip.copy(versionMessage, pos, 0);
+  var ip = buildNetAddr([127, 0, 0, 1], 18333);
+  ip.copy(versionMessage, pos); // addr recv
   pos += 26;
-  ip.copy(versionMessage, pos, 0);
+  ip.copy(versionMessage, pos); // addr from
   pos += 26;
   crypto.randomBytes(8).copy(versionMessage, pos, 0, 8); // nounce
   pos += 8;
   versionMessage[pos] = 0; // UA string (skipped)
   pos += 1;
+
 //  versionMessage[pos] = userAgentLength // UA length
 //  pos += 1;
 //  versionMessage.write("grouik", pos); // UA value
 //  pos += userAgentLength;
-  versionMessage.writeUInt32LE(0, pos); // height (last block)
+
+  versionMessage.writeUInt32LE(0, pos); // height (last known block)
 
   return versionMessage;
 }
